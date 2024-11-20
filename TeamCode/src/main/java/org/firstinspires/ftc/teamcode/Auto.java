d8005f8cc629cb8024e604eb603493674457ee90
proman678 <proman678910@gmail.com>
2024-11-19 16:40:21 -0800

    meet 2 gonna kms

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -10,58 +10,86 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import org.firstinspires.ftc.teamcode.MecanumDrive;
 
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+
+
 
 @Config
 @Autonomous(name = "IntoTheDeep", group = "Autonomous")
 public class Auto extends LinearOpMode {
+
+    public class Claw {
+        private Servo clawObj;
+
+        public Claw(HardwareMap hardwareMap){
+            clawObj = hardwareMap.get(Servo.class, "outtakeClaw");
+        }
+
+        public class openClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                clawObj.setPosition(0.7); // Set the servo to position 1
+                return false;
+            }
+        }
+
+        public class closeClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                clawObj.setPosition(0.5); // Set the servo to position 1
+                return true;
+            }
+        }
+
+        public Action openClaw(){
+            return new openClaw();
+        }
+
+        public Action closeClaw(){
+            return new closeClaw();
+        }
+
+    }
     // outtake servo 0.5 close 1 open
     // pivot outtake 1 place 0 intake
     // left pivot - left is up right is down
     // intake 0.5 close right open
     @Override
     public void runOpMode() {
-        Pose2d initialPose = new Pose2d(-36, -61.5, Math.toRadians(90));
+        Claw outtakeClaw = new Claw(hardwareMap);
+        Pose2d initialPose = new Pose2d(0, -61.5, Math.toRadians(-90));
         SparkFunOTOSDrive drive = new SparkFunOTOSDrive(hardwareMap, initialPose);
 
         // vision here that outputs position
 
-        TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
-                .waitSeconds(1)
-                .lineToY(38)
-                .waitSeconds(1)
-                .strafeTo(new Vector2d(38, 38))
-                .waitSeconds(1)
-                .lineToY(-50)
-                .waitSeconds(1)
-                .strafeTo(new Vector2d(-36, -61.5));
+        TrajectoryActionBuilder goToBar = drive.actionBuilder(initialPose)
+                .strafeTo(new Vector2d(0, -34));
 
-
+        Action goToObservation = goToBar.endTrajectory().fresh()
+                .strafeTo(new Vector2d(56, -61.5))
+                .build();
 /*
         Action trajectoryActionCloseOut = tab1.fresh()
                 .strafeTo(new Vector2d(48, 12))
                 .build();
 */
 
+        Servo clawObj = hardwareMap.get(Servo.class, "outtakeClaw");
+        clawObj.setPosition(0.3);
+        // Actions.runBlocking(outtakeClaw.closeClaw());
 
         waitForStart();
         if (isStopRequested()) return;
 
-        Action trajectoryActionChosen;
-
-
-        trajectoryActionChosen = tab1.build();
-
+        Action trajectoryActionChosen = goToBar.build();
 
         Actions.runBlocking(
                 new SequentialAction(
-                        trajectoryActionChosen
+                        trajectoryActionChosen,
+                        outtakeClaw.openClaw(),
+                        goToObservation
                 )
         );
     }
